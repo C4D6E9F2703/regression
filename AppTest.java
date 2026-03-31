5b518b63c2f9ccd2f70036f446015af950ee44d7
C4D6E9F2703 <rjrjamuna@gmail.com>
2026-03-31 21:52:00 +0530

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,12 @@
+package com.example.regression;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void myRegressionTest() {
+        System.out.println("Hello OCI! Regression Test Running...");
+        assertTrue(true);
+    }
+}
